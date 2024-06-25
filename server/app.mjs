65e75cffc3da28826896ce389da289aee297bc80
Pond <pondwachi.th@gmail.com>
2024-06-25 16:10:04 +0700

    feat: build API 2

--- server/app.mjs
+++ server/app.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import { pool } from "./utils/db.mjs"
 
 const app = express();
 const port = 4001;
@@ -7,6 +8,90 @@ app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
 
+app.get("/assignments/:assignmentId", async (req, res) => {
+  const assignFronCrlient = req.params.postId
+  const result = await pool.query(`select * from assignments where assingment_id=$1`, [assignFronCrlient])
+  try {
+    await pool.query(`select * from assignments where assingment_id=$1`, [assignFronCrlient])
+    if (!result.row[0]) {
+      return res.status(404).json({
+        message: `Server could not find a requested assignment (assingment_id: ${assignFronCrlient})`
+      })
+    }
+    return res.status(200).json({
+      data: [{ assignment_data_1 }, { assignment_data_2 }, { assignment_data_3 }]
+
+    })
+
+  } catch {
+    return res.status(500).json({
+      message: "Server could not read assignment because database connection"
+    })
+  }
+})
+
+app.put("/assignments/:assignmentId", async (req, res) => {
+  const assignIdFronCrlient = req.params.assignmentId
+  const updateAssignment = { ...req.body, updated_at: new Date() }
+  const result = await pool.query(`select * from assignments where assingment_id=$1`, [assignIdFronCrlient])
+
+  try {
+    await pool.query(`
+      update assignment
+      set title = $2
+          content = $3
+          category = $4
+          length = $5
+          status =$6
+          updated_at = $8
+      where assignment_id = $1
+      `, [
+      assignIdFronCrlient,
+      updateAssignment.title,
+      updateAssignment.content,
+      updateAssignment.category,
+      updateAssignment.length,
+      updateAssignment.status,
+      updateAssignment.updated_at,
+    ])
+    if (!result.row[0]) {
+      return res.status(500).json({
+        message: "Server could not update assignment because database connection"
+      })
+    }
+    return res.status(200).json({
+      message: "Updated assignment sucessfully"
+    })
+  } catch {
+    return res.status(404).json({
+      message: "Server could not find a requested assignment to update"
+    })
+  }
+})
+
+
+app.delete("/assignments/:assignmentId", async (req, res) =>{
+  const assignIdFronCrlient = req.params.assignmentId
+  const result = await pool.query(`select * from assignments where assingment_id=$1`, [assignIdFronCrlient])
+  try {
+    await pool.query(
+      `delete from assignments where assignmrnt_id = $1`,[assignIdFronCrlient] )
+      if (!result.row[0]) {
+        return res.status(500).json({
+          message: "Server could not delete assignment because database connection" 
+      })
+    }
+      return res.status(200).json({
+        message: "Deleted assignment sucessfully"
+      })
+  }catch {
+    return res.status(404).json({
+      message: "Server could not delete assignment because database connection"
+    })
+  }
+})
+
+
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
-});
+})
--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -1,10 +1,10 @@
 // Create PostgreSQL Connection Pool here !
 import * as pg from "pg";
-const { Pool } = pg.default;
+const { Pool } = pg.defaults;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:PondDoy1123@localhost:5432/Build the Complete CRUD APIs",
 });
 
 export default connectionPool;
